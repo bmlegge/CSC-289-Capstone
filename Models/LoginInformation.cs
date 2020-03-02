61b8c97cc4356fb49335907cec9e0ff739ec43a1
Jochs1989 <43485024+Jochs1989@users.noreply.github.com>
2020-03-02 10:44:59 -0500

    Add files via upload

---
+++ Models/LoginInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneProjectWeb.Models
+{
+    public class LoginInformation
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Pages/Forms/Login.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CapstoneProjectWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CapstoneProjectWeb.Pages.Forms
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public LoginInformation Login { get; set; }
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            // TODO Create Database Library to Save Model to Database
+
+            return RedirectToPage("/Index", new { UserName = "Coffee Finder " + Login.UserName });
+        }
+    }
+}
